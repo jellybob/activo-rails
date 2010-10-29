4c77bd60ae77c299ffd77ab8623d227197783400
Jon Wood <jon@blankpad.net>
2010-10-29 16:50:09 +0100

    Change the constant name

--- lib/activo-rails.rb
+++ lib/activo-rails.rb
@@ -1,7 +1,7 @@
 module ActivoRails
   class Engine < Rails::Engine
     config.mount_at = '/activo/'
-    ActionController::Base.helper ActivoRails::ActivoHelper
+    ActionController::Base.helper ActivoRails::Helper
     
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"

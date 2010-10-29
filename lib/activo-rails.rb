bc854894da65e8ba048b3d018a56c78232feef08
Jon Wood <jon@blankpad.net>
2010-10-29 16:49:02 +0100

    move the helpers

--- lib/activo-rails.rb
+++ lib/activo-rails.rb
@@ -1,6 +1,7 @@
 module ActivoRails
   class Engine < Rails::Engine
     config.mount_at = '/activo/'
+    ActionController::Base.helper ActivoRails::ActivoHelper
     
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"

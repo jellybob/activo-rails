a9d45a28b506a0f0f992b399d9ef6cd6dc4ef3b7
Jon Wood <jon@blankpad.net>
2010-10-29 16:41:46 +0100

    Set a mount point

--- lib/activo-rails.rb
+++ lib/activo-rails.rb
@@ -1,5 +1,7 @@
 module ActivoRails
   class Engine < Rails::Engine
+    config.mount_at = '/activo/'
+    
     initializer "static assets" do |app|
       app.middleware.use ::ActionDispatch::Static, "#{root}/public"
     end

aea5d8882cb3d6097b3528f0fe53c52f00d1b98f
Jon Wood <jon@blankpad.net>
2011-06-24 16:31:20 +0100

    Work around for asset_path not working outside a full Rails app due to Rails #1816

--- spec/helper_spec.rb
+++ spec/helper_spec.rb
@@ -9,6 +9,10 @@ require 'action_view/template/handlers/erb'
 class View < ActionView::Base
   include Activo::Rails::Helper
   include ActionView::Helpers::TagHelper
+
+  def asset_path(path)
+    File.join("/assets", path)
+  end
 end
 
 describe Activo::Rails::Helper do
